489004ad2e168bd39810af8901afb9c522303f96
Kabita <154049421+Kabitakhanal@users.noreply.github.com>
2024-02-11 21:29:44 +0545

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,51 +1,40 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Menu = () => {
-    const posts=[
-        {
-          id:1,
-          title: "October favorite recepies",
-          desc:"I have compiled a list of my favorite fall desert recepies that you can try and enjoy with your friends and family!",
-          img: "https://hips.hearstapps.com/goodhousekeeping/assets/18/03/1516373451-skillet-cookie-sundaes-ghk-0218.jpg"
-        },
-        {
-          id:2,
-          title: "Draco Malfoy a werewolf!?",
-          desc:"The latest Harry Potter conspiracy theory is here!",
-          img: "https://i2-prod.mirror.co.uk/incoming/article6363218.ece/ALTERNATES/s1200b/Draco-Malfoy-Wolf.jpg"
-        },
-        {
-          id:3,
-          title: "1989(Taylor's Version) OUT SOON!!!",
-          desc:"It will arrive just three months after ‘Speak Now (Taylor’s Version)’ delivered the biggest opening sales week of the year.",
-          img: "https://www.udiscovermusic.com/wp-content/uploads/2023/08/1989-Taylors-Version-1024x1024.jpg"
-        },
-        {
-          id:4,
-          title: "Tilicho Trek",
-          desc:"Watch Koshish Shrestha's new video about his trek to Tilicho Lake on youtube now.",
-          img: "https://i.ytimg.com/vi/YAyoDNkpPBc/maxresdefault.jpg"
-        },
-        
-      ];
+  const [randomPosts, setRandomPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchRandomPosts = async () => {
+      try {
+        // Replace 'http://example.com/api/news' with the actual API endpoint
+        const response = await axios.get('http://localhost:3001/v1/news');
+        const shuffledPosts = response.data.data.sort(() => 0.5 - Math.random());
+        const selectedPosts = shuffledPosts.slice(0, 3);
+        setRandomPosts(selectedPosts);
+      } catch (error) {
+        console.error('Error fetching random posts:', error);
+      }
+    };
+
+    fetchRandomPosts();
+  }, []);
+
   return (
     <div className='menu'>
-        <h1>Other posts you may like</h1>
-        {posts.map(post=>(
-            <div className="post" key={post.id}>
-                <img src={post.img} alt=""/>
-                <h2>
-                {post.title}
-                </h2>
-                <Link className='link' to={'/post/${post.id'}>
-              <button >
-                Read More</button>
-                </Link>
-            </div>
-        ))}
+      <h1>Other news you may like</h1>
+      {randomPosts.map(post => (
+        <div className="post" key={post.id}>
+          <img src={post.photo} alt={post.news_title} />
+          <h2>{post.news_title}</h2>
+          <Link className='link' to={`/post/${post.id}`}>
+            <button>Read More</button>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Menu
+export default Menu;
